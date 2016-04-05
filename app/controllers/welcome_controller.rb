ebccfbc486b84ca3f9b7bcda6b9e2e12c567b657
marinacor1 <marinaspam1234@gmail.com>
2016-04-04 21:43:12 -0600

    adding welcome page

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,6 @@
+class WelcomeController < ApplicationController
+  def index
+     render(text: "Welcome to the Welcome Page.")
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get '/students', to: 'students#index'
   resources :courses #create, index and edit gives show message
   resources :teachers, only: [:index, :show] #this shows show page only
+  get '/', to: 'welcome#index'
 end
