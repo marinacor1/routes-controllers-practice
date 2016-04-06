69ea7cb2eeea0a552556a3eeaeac425a89d90dbc
marinacor1 <marinaspam1234@gmail.com>
2016-04-05 20:51:43 -0600

    fixing formatting

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,7 @@
 class StudentsController < ApplicationController
 
   def index
-    all_students = Student.pluck(:first_name).join(",")
+    all_students = Student.pluck(:first_name).join(", ")
     render(text: "These are all of the students: #{all_students}. ")
   end
 
