747de8853cc5ae1811a7ad8dca29835ff8f53f9e
marinacor1 <marinaspam1234@gmail.com>
2016-04-06 07:24:26 -0600

    renaming variables in show

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,13 +6,11 @@ class StudentsController < ApplicationController
   end
 
   def show
-    # render(text: "#{params["id"]}")
-    # params = {"controller"=>"students", "action"=>"show", "id"=>"3"}
     correct_student = Student.find_by(id: params["id"])
     first_name = correct_student.first_name
-    correct_current_address = correct_student.addresses.map {|a| a.current}
-    correct_permanent_address = correct_student.addresses.map {|a| a.permanent}
-    render(text: "#{first_name}:" "#{correct_current_address}" "#{correct_permanent_address}")
+    current_addresses = correct_student.addresses.map {|a| a.current}
+    permanent_addresses = correct_student.addresses.map {|a| a.permanent}
+    render(text: "#{first_name}:" "#{current_addresses}" "#{permanent_addresses}")
   end
 
 end
